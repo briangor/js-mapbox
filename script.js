9879f8e4d805ff170861b1436a98f962b7e304de
briangor <briangor13@gmail.com>
2024-03-11 22:08:57 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//import { testFn } from "./test";
+
+//mapboxgl.accessToken = process.env.ACCESS_TOKEN;
+
+let map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11"
+});
+
+//map.on('load', testFn);
