f292baa0d2f0e5911a69071648e141441c37f8b6
briangor <briangor13@gmail.com>
2024-03-12 19:56:48 +0300

    add direction

--- script.js
+++ script.js
@@ -2,9 +2,35 @@
 
 //mapboxgl.accessToken = process.env.ACCESS_TOKEN;
 
-let map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11"
-});
+// get loc
+navigator.geolocation
+.getCurrentPosition(successLoc, errorLoc, { enableHighAccuracy: true });
+
+function successLoc(position) {
+    //console.log(position);
+    setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLoc() {
+    setupMap([-2.24, 53.48]);
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: center,
+        zoom: 14
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+
+    const directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    });
+
+    map.addControl(directions, 'top-left');
+}
 
 //map.on('load', testFn);
